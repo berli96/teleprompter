3a90dd31c66c01bd548db3d3bb6b9bc03a0bd36d
Berlianto <berlianto@skycoinmail.com>
2019-06-19 10:38:06 +0700

    unignore build dir

---
+++ client/build/precache-manifest.77810d30b9f68ab9e1320c71018370f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c666a81820e4dc82c76600714e6dfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8da6605728b7eec1888f",
+    "url": "/static/css/2.b0f64131.chunk.css"
+  },
+  {
+    "revision": "533da074cc248500e36a",
+    "url": "/static/css/main.2466d321.chunk.css"
+  },
+  {
+    "revision": "8da6605728b7eec1888f",
+    "url": "/static/js/2.890c80e2.chunk.js"
+  },
+  {
+    "revision": "533da074cc248500e36a",
+    "url": "/static/js/main.3a32dee8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
