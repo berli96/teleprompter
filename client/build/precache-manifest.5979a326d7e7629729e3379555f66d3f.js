adbb5d53cd64c88535aa20ff837bbeeb7cbffafe
Berlianto <berlianto@skycoinmail.com>
2019-06-19 10:50:29 +0700

    chunks

---
+++ client/build/precache-manifest.5979a326d7e7629729e3379555f66d3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "688dbf42b762fd009b1173909efa454d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8da6605728b7eec1888f",
+    "url": "/static/css/2.b0f64131.chunk.css"
+  },
+  {
+    "revision": "096fe8fab902cecbc175",
+    "url": "/static/css/main.2466d321.chunk.css"
+  },
+  {
+    "revision": "8da6605728b7eec1888f",
+    "url": "/static/js/2.890c80e2.chunk.js"
+  },
+  {
+    "revision": "096fe8fab902cecbc175",
+    "url": "/static/js/main.c0fd4ceb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
