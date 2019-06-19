2ad0794d68048290c2cdd4c6d874459429840553
Berlianto <berlianto@skycoinmail.com>
2019-06-19 10:35:20 +0700

    change server port

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ io.on('connection', socket => {
   });
 });
 
-http.listen(process.env.port || 8080, () => {
+http.listen(process.env.PORT || 8080, () => {
   console.log('server is running');
 });
